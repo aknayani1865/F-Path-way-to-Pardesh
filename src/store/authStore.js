83ecc04098927c48a30ee546480e33e7bf999975
Akshay Nayani <166915838+aknayani1865@users.noreply.github.com>
2024-12-07 10:56:24 +0530

    Update authStore.js

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 // const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://b-path-way-to-pardesh.onrender.com/api/auth";
 
 axios.defaults.credentials = true;
 
