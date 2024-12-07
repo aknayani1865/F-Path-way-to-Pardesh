b71717adde49eef696f3f12a0483d44d5bf3b8c3
Akshay Nayani <166915838+aknayani1865@users.noreply.github.com>
2024-12-07 10:49:57 +0530

    Update ViewHotel.js

--- src/components/ViewHotel.js
+++ src/components/ViewHotel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ViewItems from './ViewItems';
 
 const ViewHotels = () => {
-  return <ViewItems itemType="Hotels" apiEndpoint="http://localhost:5000/api/admin/hotels" />;
+  return <ViewItems itemType="Hotels" apiEndpoint="https://b-path-way-to-pardesh.onrender.com/api/admin/hotels" />;
 };
 
 export default ViewHotels;
